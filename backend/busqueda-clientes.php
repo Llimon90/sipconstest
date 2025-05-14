a7fe8c33a299f59a0764cf50f9977cc04b0aad6e
Luis Limon <Luis.Limon.iml@gmail.com>
2025-05-14 13:06:17 -0700

    pruebas de busqueda clientes 3

--- backend/busqueda-clientes.php
+++ backend/busqueda-clientes.php
@@ -3,7 +3,15 @@ require_once 'conexion.php';
 
 if (isset($_POST['consulta'])) {
     $consulta = $conexion->real_escape_string($_POST['consulta']);
-    $sql = "SELECT nombre, rfc, direccion, telefono, contactos, email FROM clientes WHERE nombre LIKE '%$consulta%' LIMIT 5";
+    $sql = "SELECT nombre, rfc, direccion, telefono, contactos, email 
+            FROM clientes 
+            WHERE nombre LIKE '%$consulta%' 
+               OR rfc LIKE '%$consulta%' 
+               OR direccion LIKE '%$consulta%' 
+               OR telefono LIKE '%$consulta%' 
+               OR contactos LIKE '%$consulta%' 
+               OR email LIKE '%$consulta%' 
+            LIMIT 10";
     $resultado = $conexion->query($sql);
 
     if ($resultado->num_rows > 0) {
