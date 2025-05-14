15ad1ab6323793b066060d60957026dc81bd66b2
Luis Limon <Luis.Limon.iml@gmail.com>
2025-05-14 12:39:18 -0700

    SE CARGAN ARCHIVOS PARA BUSQUEDA DE CLIENTES EN TIEMPO REAL 2

--- backend/busqueda-clientes.php
+++ backend/busqueda-clientes.php
@@ -16,4 +16,25 @@ if (isset($_POST['consulta'])) {
         echo '<p>No se encontraron resultados</p>';
     }
 }
+   // **INICIO - FUNCIÓN PARA MOSTRAR LA BASE DE DATOS EN EL DOM**
+    
+    // Consulta para obtener todas las incidencias sin filtrar
+    $sql = "SELECT * FROM incidencias WHERE estatus IN ('Abierto','Asignado', 'Pendiente', 'Completado') ORDER BY numero_incidente DESC; ";
+
+
+
+
+
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $incidencias = [];
+        while($row = $result->fetch_assoc()) {
+            $incidencias[] = $row;
+        }
+        echo json_encode($incidencias);
+    } else {
+        echo json_encode(["message" => "No hay incidencias abiertas"]);
+    }
 ?>
