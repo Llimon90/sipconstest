c2bde087defaca016a8ffb206f25b94bff352cff
Luis Limon <Luis.Limon.iml@gmail.com>
2025-05-22 14:43:13 -0700

    se actualiza portal de incidencias para agregar a mas 1 solo tecnico

--- scripts/script.js
+++ scripts/script.js
@@ -4,36 +4,44 @@
 
 
 document.getElementById('new-incidencia-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir envío por defecto
-    
-    const nuevaIncidencia = {
-        numero: document.getElementById('numero').value,
-        cliente: document.getElementById('cliente').value,
-        contacto: document.getElementById('contacto').value,
-        sucursal: document.getElementById('sucursal').value,
-        falla: document.getElementById('falla').value,
-        fecha: document.getElementById('fecha').value,
-        tecnico: document.getElementById('tecnico').value,
-        status: document.getElementById('estatus').value,
-        notas: document.getElementById('notas').value,
-    };
+  event.preventDefault(); // Prevenir envío por defecto
 
-    fetch('../backend/server.php', { 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(nuevaIncidencia),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor:', data);
-        alert(data.message || data.error);
-    })
-    .catch(error => {
-        console.error('Error al enviar los datos:', error);
-        alert('Hubo un error al enviar los datos');
-    });
+  // Obtener todos los técnicos seleccionados
+  const tecnicosSelects = document.querySelectorAll('select[name="tecnicos[]"]');
+  const tecnicos = [];
+  tecnicosSelects.forEach(select => {
+    if (select.value) {
+      tecnicos.push(select.value);
+    }
+  });
+
+  const nuevaIncidencia = {
+    numero: document.getElementById('numero').value,
+    cliente: document.getElementById('cliente').value,
+    contacto: document.getElementById('contacto').value,
+    sucursal: document.getElementById('sucursal').value,
+    falla: document.getElementById('falla').value,
+    fecha: document.getElementById('fecha').value,
+    tecnicos: tecnicos,
+    status: document.getElementById('estatus').value,
+    notas: document.getElementById('notas').value,
+  };
+
+  fetch('../backend/server.php', { 
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(nuevaIncidencia),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Respuesta del servidor:', data);
+    alert(data.message || data.error);
+  })
+  .catch(error => {
+    console.error('Error al enviar los datos:', error);
+    alert('Hubo un error al enviar los datos');
+  });
 });
-
 
 
 //FUNCION PARA TENER FECHA ACTUAL EN INPUT FECHA INCIDENCIA
